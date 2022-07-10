24c984a89bb077a3dbf5b0f1e71fa2b3d68f6769
shkim <skok1025>
2022-07-10 14:28:01 +0900

    Initial commit

---
+++ src/main/java/com/cafe24/shkim30/ShBlogProjectWebInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.cafe24.shkim30;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShBlogProjectWebInterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShBlogProjectWebInterfaceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/cafe24/shkim30/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.cafe24.shkim30.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/basic-template")
+    public String basicTemplate() {
+        return "basic-template";
+    }
+}
