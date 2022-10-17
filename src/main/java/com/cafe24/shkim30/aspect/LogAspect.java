e470cdc3227fd7ead325743562ed8744415ebe1d
shkim <skok1025@naver.com>
2022-10-17 13:10:19 +0900

    블로그 호출 trace 작업 - around 로 처리

--- src/main/java/com/cafe24/shkim30/aspect/LogAspect.java
+++ src/main/java/com/cafe24/shkim30/aspect/LogAspect.java
@@ -5,10 +5,8 @@ import com.cafe24.shkim30.library.trace.TraceStatus;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -24,32 +22,44 @@ public class LogAspect {
 
     private final LogTrace trace;
 
-    @Before("execution(* *..controller.*.*(..)) || execution(* *..service.*.*(..))")
-    public void beforeController(JoinPoint joinpoint) {
-        String className = joinpoint.getTarget().getClass().getName();
+//    @Before("execution(* *..controller.*.*(..)) || execution(* *..service.*.*(..))")
+//    public void beforeController(JoinPoint joinpoint) {
+//        String className = joinpoint.getTarget().getClass().getName();
+//
+//        MethodSignature signature = (MethodSignature)joinpoint.getSignature();
+//        String methodName = signature.getMethod().getName();
+//
+//        TraceStatus logStatus = trace.begin(className + "::" + methodName);
+//
+//        HttpSession session = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest().getSession();
+//
+//        session.setAttribute("logStatus::"+Thread.currentThread().toString() + "::" + className + "::" + methodName, logStatus);
+//    }
+//
+//    @After("execution(* *..controller.*.*(..)) || execution(* *..service.*.*(..))")
+//    public void afterExec(JoinPoint joinpoint) {
+//        String className = joinpoint.getTarget().getClass().getName();
+//
+//        MethodSignature signature = (MethodSignature)joinpoint.getSignature();
+//        String methodName = signature.getMethod().getName();
+//
+//        HttpSession session = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest().getSession();
+//        TraceStatus logStatus = (TraceStatus) session.getAttribute("logStatus::"+Thread.currentThread().toString() + "::" + className + "::" + methodName);
+//        trace.end(logStatus);
+//
+//        session.removeAttribute("logStatus::"+Thread.currentThread().toString() + "::" + className + "::" + methodName);
+//    }
 
-        MethodSignature signature = (MethodSignature)joinpoint.getSignature();
-        String methodName = signature.getMethod().getName();
+    @Around("execution(* *..controller.*.*(..)) || execution(* *..service.*.*(..)) || execution(* *..providor.*.*(..))")
+    public Object setLogTrace(ProceedingJoinPoint pjp) throws Throwable {
+        String targetName = pjp.getTarget().getClass().getName() + " :: " + pjp.getSignature().getName();
+        TraceStatus logStatus = trace.begin(targetName);
 
-        TraceStatus logStatus = trace.begin(className + "::" + methodName);
+        Object result = pjp.proceed(); // method 실행
 
-        HttpSession session = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest().getSession();
-
-        session.setAttribute("logStatus::"+Thread.currentThread().toString() + "::" + className + "::" + methodName, logStatus);
-    }
-
-    @After("execution(* *..controller.*.*(..)) || execution(* *..service.*.*(..))")
-    public void afterExec(JoinPoint joinpoint) {
-        String className = joinpoint.getTarget().getClass().getName();
-
-        MethodSignature signature = (MethodSignature)joinpoint.getSignature();
-        String methodName = signature.getMethod().getName();
-
-        HttpSession session = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest().getSession();
-        TraceStatus logStatus = (TraceStatus) session.getAttribute("logStatus::"+Thread.currentThread().toString() + "::" + className + "::" + methodName);
         trace.end(logStatus);
 
-        session.removeAttribute("logStatus::"+Thread.currentThread().toString() + "::" + className + "::" + methodName);
+        return result;
     }
 
     @AfterThrowing(pointcut = "execution(* *..controller.*.*(..)) || execution(* *..service.*.*(..))", throwing = "e")
